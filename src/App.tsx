7f3381a847442a6e586307eadc16860279828d1d
gopinath-programmer <gopi63669@gmail.com>
2024-10-01 16:38:26 +0530

    import FolderCard Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserCreate from './minimals/user-create/UserCreate';
-import CourseSelection from './minimals/user-create/featured-course/CourseSelection';
+import FolderCard from './minimals/folder-card/FolderCard';
+
 
 function App() {
   return (
-    // <UserCreate/>
-     <CourseSelection />
+    <FolderCard />
+
   );
 }
 
